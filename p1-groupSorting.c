b700770492c83b4fa9f05139786b39d64eb08b60
Riasalit <daniel-vilslev@hotmail.com>
2018-11-23 12:21:34 +0100

    Update p1-groupSorting.c

--- p1-groupSorting.c
+++ p1-groupSorting.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef enum {nothing, ideskaber, specialist, analysator, organisator, opstarter, afslutter, formidler, koordinator, kontaktskaber} roles;
-
 int main(void){
 
   return EXIT_SUCCESS;
 }
+
