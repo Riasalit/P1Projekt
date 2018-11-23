a789763a6df2165db2165d43df9bfea839ec9edd
Riasalit <daniel-vilslev@hotmail.com>
2018-11-23 12:22:07 +0100

    Add files via upload

---
+++ structs.h
@@ -0,0 +1,8 @@
+
+typedef enum {nothing, ideskaber, specialist, analysator, organisator, opstarter, afslutter, formidler, koordinator, kontaktskaber} roles;
+
+typedef struct{
+  char name[30];
+  int id;
+  roles role[];
+} student;
