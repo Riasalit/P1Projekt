60a8ab4597524f05ae0a89b7a071089f0de5b7f5
Riasalit <daniel-vilslev@hotmail.com>
2018-12-05 10:59:05 +0100

    Create makeStudentArray.h

---
+++ makeStudentArray.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "structs.h"
+
+void makeStudentArray(const FILE *dataSet, const int nrOfStudents, student *class){
+  /*nrOfStudents kommer fra countStudents*/
+
+}
