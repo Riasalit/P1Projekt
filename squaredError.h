1fe222087ffc262b6effdce244754bb1ae57003d
Riasalit <daniel-vilslev@hotmail.com>
2018-12-05 11:01:11 +0100

    Create squaredError.h

---
+++ squaredError.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "structs.h"
+
+int squaredError(const int groupSize, const int nrOfStudents, const int nrOfGroups, group *groups){
+  /*find gennemsnit*/
+  /*Kald i loop med resortNormies */
+  /*return 0 er placeholder*/
+  return 0;
+}
