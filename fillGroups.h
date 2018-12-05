742d56faaf180b7492705c69c9c286619d6e5719
Riasalit <daniel-vilslev@hotmail.com>
2018-12-05 11:00:22 +0100

    Create fillGroups.h

---
+++ fillGroups.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "structs.h"
+
+void fillGroups(const student *class, const int groupSize, const int nrOfStudents, const int nrOfGroups, group *groups){
+  /*"for (hver student){
+    hvis den gør mere i en gruppe gem gruppens indeks
+    set i gruppen den gjorde mest
+  }"*/
+}
