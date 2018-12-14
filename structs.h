064c399bf870c209e4d3f362433d814f3898f181
Riasalit <daniel-vilslev@hotmail.com>
2018-12-14 11:26:12 +0100

    Update structs.h

--- structs.h
+++ structs.h
@@ -11,13 +11,11 @@ typedef enum {nothing, ideskaber,
 typedef struct{
   int isStudent;/*is used as boolean value for arrays of students which hasn't been filled out yet*/
   char name[30];/*students name*/
-  int id; /*Student id*/
   roles role[ROLES_IN_STUDENT]; /*The three main roles a student has*/
 } student;
 
 typedef struct{
   student *students;/*pointer meant to point to array of every student in group, dynamically allocated later*/
-  int quality;
   int studentsInGroup; /*the amount of students in the group*/
   int amountOfRoles; /*the amount of unique roles represented in the group*/
   roles roles[ROLES_IN_GROUP]; /*array containing every unique role in a group*/
