10d4544a9350eeba4f1da902c0cd98e9acbf4789
Riasalit <daniel-vilslev@hotmail.com>
2018-12-16 00:06:32 +0100

    Add files via upload

--- printAll.h
+++ printAll.h
@@ -1,8 +1,16 @@
 void printAll(const group *groups, const int groupSize, const int nrOfGroups){
   int i, j;
+  FILE *opf;
+  opf = fopen("Groups-Comple.txt", "w");
+  if(opf == NULL){
+    printf("dataSet could not be found\n");
+    exit(EXIT_FAILURE);
+  }
   for(i = 0; i < nrOfGroups; i++){/*iterate through every group*/
+    fprintf(opf,"\n--------------------------------------------------------\n Group %d\n--------------------------------------------------------\n", i+1);
     printf("\n Group %d score = %d\n--------------------------------------------------------\n", i+1, groups[i].amountOfRoles);/*print group number and score*/
     for(j = 0; j < groups[i].studentsInGroup; j++){/*iterate through every student in the group*/
+      fprintf(opf, "%s\n", groups[i].students[j].name);
       printf("%s: roles = %d, %d, %d\n", groups[i].students[j].name,
                                          groups[i].students[j].role[0],
                                          groups[i].students[j].role[1],
