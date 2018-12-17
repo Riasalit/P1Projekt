2e2d59e196b0d85b0b42f4618ba840344ba33ab4
Riasalit <daniel-vilslev@hotmail.com>
2018-12-17 11:32:23 +0100

    Update allocateStudents.h

--- allocateStudents.h
+++ allocateStudents.h
@@ -2,7 +2,7 @@ student *allocateStudents(const int nrOfStudents){
   student *temp;
   temp = calloc(nrOfStudents, sizeof(student));/*allocate space for every student*/
   if(temp == NULL){/*display error message if not enough space could be allocated and shut down program*/
-    printf("ERROR: Could not create space for temp");
+    printf("ERROR: Could not create space for temp\n");
     exit(EXIT_FAILURE);
   }
   return temp;
