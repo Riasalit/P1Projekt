d7a8cecee3d4599ad769cf9be21ab7924b6f00c3
Riasalit <daniel-vilslev@hotmail.com>
2018-12-17 11:31:00 +0100

    Update allocateBestGroups.h

--- allocateBestGroups.h
+++ allocateBestGroups.h
@@ -4,14 +4,14 @@ group *allocateBestGroups(const int nrOfStudents, const int groupSize, const int
   temp = NULL;
   temp = calloc(nrOfGroups, sizeof(group));/*allocates space for all the groups needed*/
   if(temp == NULL){/*display error message if not enough space could be allocated and shut down program*/
-    printf("ERROR: Could not create space for temp");
+    printf("ERROR: Could not create space for temp\n");
     exit(EXIT_FAILURE);
   }
   for(i = 0; i < nrOfGroups; i++){/*iterate through all the groups*/
     temp[i].students = NULL;/*makes a student into NULL pointer*/
     temp[i].students = calloc(groupSize, sizeof(student));/*allocates space for every student*/
     if(temp[i].students == NULL){/*display error message if not enough space could be allocated and shut down program*/
-      printf("ERROR: Could not create space for temp[%d].student", i);
+      printf("ERROR: Could not create space for temp[%d].student\n", i);
       exit(EXIT_FAILURE);
     }
   }
